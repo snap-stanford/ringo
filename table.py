78384a868caa4f164f2c3733f924eb0258393859
Martin Raison <martinraison@gmail.com>
2013-02-02 15:53:48 -0800

    Added XML support and exporting data as TSV file
    Table.load reads XML files where rows are specified with the "row" tag
    (like in StackOverflow data)
    Strings in TSV files are encoded with 'unicode-escape' (to avoid issues
    with newline, carriage-return, etc)

--- table.py
+++ table.py
@@ -1,4 +1,8 @@
 
+import csv
+import os
+from xml.dom.minidom import parse
+
 class Table:
 
 	def __init__(self, name):
@@ -11,13 +15,43 @@ class Table:
 			self.attributes.append(attr)
 
 	def load(self, filename):
+		"""
+		Load data from either XML or TSV file
+		"""
 		f = open(filename, 'r')
-		for line in f:
-			row = []
-			values = line.split('\t')
-			for v in values:
-				row.append(v)
-			self.data.append(row)
+		_,ext = os.path.splitext(filename);
+		if ext=='.xml':
+			# Parse the XML file
+			dom = parse(f)
+			f.close()
+			xmlrows = dom.getElementsByTagName('row')
+			first = True
+			for xmlrow in xmlrows:
+				a = xmlrow.attributes
+				if first:
+					# Create the list of attributes
+					for i in range(a.length):
+						self.attributes.append(a.item(i).name)
+					first = False
+				row = []
+				for i in range(a.length):
+					row.append(a.item(i).value)
+				self.data.append(row)
+		else:
+			# Assume the input is a TSV file
+			data = csv.reader(f,delimiter='\t')
+			for row in data:
+				self.data.append([cell.decode('unicode-escape') for cell in row])
+
+	def write(self, filename):
+		"""
+		Write the data to a TSV file
+		"""
+		f = open(filename,'wb')
+		wr = csv.writer(f,delimiter='\t')
+		for row in self.data:
+			wr.writerow([cell.encode('unicode-escape') for cell in row])
+		f.close()
 
 	def name(self):
 		return self.name
