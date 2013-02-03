b4e093db082fae1c73aa23f5bcbcaa801746eb8e
Martin Raison <martinraison@gmail.com>
2013-02-03 12:14:54 -0800

    Improved XML/TSV support
    The import function automatically recognizes attribute types
    (number,string,date) and converts strings accordingly
    Supports sparse XML tables (attributes undefined on certain rows)
    Added types array to Table object, storing attribute types

--- table.py
+++ table.py
@@ -1,6 +1,7 @@
 
 import csv
 import os
+import time
 from xml.dom.minidom import parse
 
 class Table:
@@ -8,6 +9,7 @@ class Table:
 	def __init__(self, name):
 		self.name = name
 		self.attributes = []
+		self.types = []
 		self.data = []
 
 	def set_attributes(self, attributes):
@@ -25,32 +27,62 @@ class Table:
 			dom = parse(f)
 			f.close()
 			xmlrows = dom.getElementsByTagName('row')
-			first = True
 			for xmlrow in xmlrows:
-				a = xmlrow.attributes
-				if first:
-					# Create the list of attributes
-					for i in range(a.length):
-						self.attributes.append(a.item(i).name)
-					first = False
+				attr = xmlrow.attributes
+				for i in range(attr.length):
+					name = attr.item(i).name
+					if not name in self.attributes:
+						# Add column to the table
+						elt = Table.str2elt(attr.item(i).value)
+						self.attributes.append(name)
+						self.types.append(type(elt))
+						for r in self.data:
+							r.append(None)
 				row = []
-				for i in range(a.length):
-					row.append(a.item(i).value)
-				self.data.append(row)
+				row = [xmlrow.getAttribute(name) for name in self.attributes]
+				self.addrow(row)
 		else:
 			# Assume the input is a TSV file
 			data = csv.reader(f,delimiter='\t')
+			init = True
 			for row in data:
-				self.data.append([cell.decode('unicode-escape') for cell in row])
+				if init:
+					self.attributes = ['']*len(row)
+					self.types = [type(None)]*len(row)
+					init = False
+				self.addrow([cell.decode('unicode-escape') for cell in row])
+
+	def addrow(self, strrow):
+		assert len(strrow) == len(self.attributes)
+		row = []
+		for i in range(len(strrow)):
+			e = Table.str2elt(strrow[i])
+			if self.types[i] == type(None):
+				# Initialize type
+				self.types[i] = type(e)
+				row.append(e)
+			elif type(e) is self.types[i] or e is None:
+				row.append(e)
+			else:
+				# Convert all other values in the column back to string
+				row.append(strrow[i])
+				for r in self.data:
+					if not r[i] is None:
+						r[i] = unicode(r[i])
+				self.types[i] = unicode
+		self.data.append(row)
 
 	def write(self, filename):
 		"""
 		Write the data to a TSV file
 		"""
 		f = open(filename,'wb')
-		wr = csv.writer(f,delimiter='\t')
+		wr = csv.writer(f,delimiter='\t',quoting=csv.QUOTE_ALL)
 		for row in self.data:
-			wr.writerow([cell.encode('unicode-escape') for cell in row])
+			strrow = []
+			for cell in row:
+				strrow.append(Table.elt2str(cell))
+			wr.writerow(strrow)
 		f.close()
 
 	def name(self):
@@ -65,3 +97,26 @@ class Table:
 			result.add(row[attr])
 
 		return result
+
+	@staticmethod
+	def str2elt(s):
+		if s == '':
+			return None
+		try:
+			return float(s)
+		except ValueError:
+			try:
+				# TODO: use datetime module to keep milliseconds
+				spl = s.split('.')
+				return time.strptime(spl[0],'%Y-%m-%dT%H:%M:%S')
+			except ValueError:
+				return s
+
+	@staticmethod
+	def elt2str(e):
+		if e is None:
+			return ''
+		elif type(e) is time.struct_time:
+			return time.strftime('%Y-%m-%dT%H:%M:%S',e)
+		else:
+			return unicode(e).encode('unicode-escape')
