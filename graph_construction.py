49878ccbeb7894a6fa61b595fe8f3fac6471801e
dilli <drpaudel@gmail.com>
2013-02-05 15:25:11 -0800

    implemented RA stubs

--- graph_construction.py
+++ graph_construction.py
@@ -9,33 +9,66 @@ def project(table, columns):
 	return result
 
 def group(table, attributes, aggregation_attributes, aggregation_function):
-	result = {}
+	result = []
 
 	for row in table:
 		for attr in aggregation_attributes:
-			aggregation_function(row)
+			result.append(table.aggregate(aggregation_function, attr))
 
 	return result
 
 def union(table1, table2, condition):
 	result = {}
-	for row in table1:
-		if row.condition == condition:
-			result.add(row)
+	for row in table1.getTuples(condition):
+		result.add(row)
 
-	for row in table2:
-		if row.condition == condition:
-			result.add(condition)
+	for row in table2.getTuples(condition):
+		result.add(condition)
 
 	return result
 
-def join(table1, table2, condition):
+def join(table1, table2, col):
+	result = []
+	for row in table1:
+		newrow = []
+		for row2 in table2:
+			if (row1[col] == row2[col]):
+				for elem in row1:
+					newrow.append(elem)
+				for elem in row2:
+					if elem != row2[col]:
+						newrow.append(elem)
+		if len(newrow) > 0:
+			result.append(newrow)
 
-def rename:
+	return result
 
-def intersect:
+def rename(table, oldattr, newattr):
+	table.setAttr(oldattr, newattr)
 
-def diff:
+def intersect(table1, table2, condition):
+	result = []
+	intermediate = []
+	for row in table1.getTuples(condition):
+		intermediate.add(row)
+
+	for row in table2.getTuples(condition):
+		if row in intermediate:
+			result.add(row)
+
+	return result
+
+def diff(table1, table2, condition):
+	result = []
+	intermediate = []
+	for row in table1.getTuples(condition):
+		intermediate.add(row)
+
+	for row in table2.getTuples(condition):
+		if row not in intermediate:
+			result.add(row)
+
+	return result
 
 def makeGraph(table1, table2, table3):
 	
