8ac86c52d48daa2852f844d1ec94eb0906ed479c
Martin Raison <martinraison@gmail.com>
2013-10-22 11:46:24 -0700

    Added test to simple.py

--- pytest/simple.py
+++ pytest/simple.py
@@ -9,3 +9,9 @@ S.Add(P2)
 Context = snap.TTableContext()
 T = snap.TTable.LoadSS(1, S, "test.tsv", Context)
 
+assert T.GetIntVal("col1",0) == 5 \
+    and T.GetIntVal("col1",1) == 12 \
+    and T.GetStrVal("col2",0) == "test" \
+    and T.GetStrVal("col2",1) == "test2", \
+    "Test error: TTable does not contain the expected data"
+print "Test OK"
