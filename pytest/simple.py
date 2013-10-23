1fe35a0ca4e2fe53a79e04bb60e8efe9ff399ab2
Rok Sosic <rok@cs.stanford.edu>
2013-10-23 01:20:44 -0700

    updated simple.py so that it executes correctly

--- pytest/simple.py
+++ pytest/simple.py
@@ -1,20 +1,23 @@
 import snap
 
-P1 = snap.TStrIntPr("col1", 1)
-P2 = snap.TStrIntPr("col2", 2)
+P1 = snap.TStrTAttrPr("col1", snap.atInt)
+P2 = snap.TStrTAttrPr("col2", snap.atStr)
 S = snap.Schema()
 S.Add(P1)
 S.Add(P2)
 
 Context = snap.TTableContext()
-T = snap.TTable.LoadSS(1, S, "test.tsv", Context)
+T = snap.TTable.LoadSS("1", S, "test.tsv", Context)
 
-data = [T.GetIntVal("col1",0), \
-    T.GetIntVal("col1",1), \
-    T.GetStrVal("col2",0), \
-    T.GetStrVal("col2",1)]
-print "Data: " + str(data)
-if not data == [5, 12, "test", "test2"]:
+c10 = T.GetIntVal("col1",0)
+c11 = T.GetIntVal("col1",1)
+c20 = T.GetStrVal("col2",0)
+c21 = T.GetStrVal("col2",1)
+
+print "c10 %d, c11 %d, c20 %s, c21 %s" % (c10, c11, c20, c21)
+
+if c10 != 5  or  c11 != 12  or  c20 != "test"  or c21 != "test2":
   print "Test error"
 else:
   print "Test OK"
+
