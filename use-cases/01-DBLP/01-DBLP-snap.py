3ae62c5a47b2c027c07f5ab7d7c8c0dfca4fdc83
Rok Sosic <rok@cs.stanford.edu>
2013-10-30 16:04:11 -0700

    updated directory and file names

---
+++ use-cases/01-DBLP/01-DBLP-snap.py
@@ -0,0 +1,46 @@
+"""
+ Use case #1: create a coauthorship network and print its estimated diameter
+"""
+
+import sys
+sys.path.append("..")
+sys.path.append("../../ringo-engine-python")
+import time
+import snap
+import ringo2
+import testutils
+import pdb
+
+N_TEST_NODES = 10
+ENABLE_TIMER = True
+
+if len(sys.argv) < 2:
+  print """Usage: python use_case_1.py source [destination]
+  source: input DBLP .tsv file
+  destination: file where the coauthorship network should be stored"""
+  exit(1)
+srcfile = sys.argv[1]
+dstfile = sys.argv[2] if len(sys.argv) >= 3 else None
+
+ringo = ringo2.ringo()
+
+t = testutils.Timer(ENABLE_TIMER)
+S = {"Key":"string", "Author":"string"}
+T = ringo.LoadTableTSV(S, srcfile)
+t.show("load")
+
+T = ringo.SelfJoin(T, "Key")
+t.show("join")
+
+# TODO: use simpler conventions for column renaming
+G = ringo.ToGraph(T, "1_1.Author","1_2.Author")
+t.show("graph")
+
+if not dstfile is None:
+  G.Save(snap.TFOut(dstfile))
+  t.show("save")
+
+diameter = snap.GetBfsEffDiam(G,N_TEST_NODES)
+t.show("diameter (%d test nodes)" % N_TEST_NODES)
+
+print "Diameter: {0:.5f}".format(diameter)
