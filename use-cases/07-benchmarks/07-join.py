3175a8a777e1b840bd0a26ca8b6d9885c01d75e0
Rok Sosic <rok@cs.stanford.edu>
2013-12-01 15:35:00 -0800

    merged the two join benchmarks into one with an additional parameter for the second table

---
+++ use-cases/07-benchmarks/07-join.py
@@ -0,0 +1,40 @@
+import os
+import resource
+import sys
+import time
+import pdb
+
+sys.path.append("../utils")
+
+import snap
+import testutils
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 3:
+        print "Usage: " + sys.argv[0] + " <srcfile1> <srcfile2>"
+        sys.exit(1)
+
+    srcfile1 = sys.argv[1]
+    srcfile2 = sys.argv[2]
+
+    context = snap.TTableContext()
+
+    t = testutils.Timer()
+    r = testutils.Resource()
+
+    FIn = snap.TFIn(srcfile1)
+    t1 = snap.TTable.Load(FIn, context)
+    t.show("load bin", t1)
+    r.show("__loadbin__")
+
+    schema = snap.Schema()
+    schema.Add(snap.TStrTAttrPr("Index", snap.atInt))
+    t2 = snap.TTable.LoadSS("2", schema, srcfile2, context, "\t", snap.TBool(False))
+    t.show("load text", t2)
+    r.show("__loadtext__")
+
+    t3 = t1.Join("Src", t2, "Index")
+    t.show("join", t3)
+    r.show("__join__")
+
