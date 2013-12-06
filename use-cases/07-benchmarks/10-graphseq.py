c5e43b58ac0c5250217f28fdf0d2933074ad8159
Martin Raison <martinraison@gmail.com>
2013-12-06 11:36:07 -0800

    removed pdb.set_trace()

--- use-cases/07-benchmarks/10-graphseq.py
+++ use-cases/07-benchmarks/10-graphseq.py
@@ -39,7 +39,6 @@ if __name__ == '__main__':
     t.show("join", table)
     r.show("__join__")
 
-    pdb.set_trace()
     table.SetSrcCol("1_1.OwnerUserId")
     table.SetDstCol("1_2.OwnerUserId")
     gseq = table.ToGraphSequence("1_1.CreationDate", snap.aaFirst, WINDOW_SIZE, WINDOW_SIZE)
