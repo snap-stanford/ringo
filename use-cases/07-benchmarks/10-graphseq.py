c904138b15b2b48c98d89b9f843e729eec4bdab1
Martin Raison <martinraison@gmail.com>
2013-12-06 15:49:34 -0800

    Fixed issue in ToGraphSequence benchmark

--- use-cases/07-benchmarks/10-graphseq.py
+++ use-cases/07-benchmarks/10-graphseq.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     schema.Add(snap.TStrTAttrPr("Id", snap.atInt))
     schema.Add(snap.TStrTAttrPr("OwnerUserId", snap.atInt))
     schema.Add(snap.TStrTAttrPr("AcceptedAnswerId", snap.atInt))
-    schema.Add(snap.TStrTAttrPr("CreationDate", snap.atStr))
+    schema.Add(snap.TStrTAttrPr("CreationDate", snap.atInt))
     schema.Add(snap.TStrTAttrPr("Score", snap.atInt))
     table = snap.TTable.LoadSS("1", schema, srcfile, context, "\t", snap.TBool(False))
     t.show("load text", table)
