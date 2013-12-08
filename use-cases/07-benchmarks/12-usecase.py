d891fd13fece1ac027be8e1a9d33c8f5c0d9c4e5
Pararth Shah <pararthshah717@gmail.com>
2013-12-08 00:44:10 -0800

    Update select prototype in benchmarking script

--- use-cases/07-benchmarks/12-usecase.py
+++ use-cases/07-benchmarks/12-usecase.py
@@ -36,9 +36,8 @@ if __name__ == '__main__':
     t.show("load posts text", table)
     r.show("__loadpoststext__")
 
-    Selected = snap.TIntV()
-    table.SelectAtomicStrConst("Tag", "python", snap.EQ, Selected, snap.TBool(False))
-    questions = snap.TTable.New(table, "2", Selected)
+    questions = snap.TTable.New(snap.TStr("2"), table.GetSchema(), context)
+    table.SelectAtomicStrConst("Tag", "python", snap.EQ, questions)
     t.show("selected tag = 'python'", questions)
     r.show("__selectedtagpython__")
 
